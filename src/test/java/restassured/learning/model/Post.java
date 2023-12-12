21d0877879c7b111a00f059126e49442cb13d2d2
Tomasz <feat24@wp.pl>
2023-12-12 08:02:58 +0100

    Add addPostObjectTest

---
+++ src/test/java/restassured/learning/model/Post.java
@@ -0,0 +1,32 @@
+package restassured.learning.model;
+
+public class Post {
+
+    int userId;
+    String title;
+    String body;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
