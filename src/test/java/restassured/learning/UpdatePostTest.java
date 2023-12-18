9faa20e30490e46ae9f52fd4ca34ca6714aaba16
Tomasz <feat24@wp.pl>
2023-12-18 07:27:19 +0100

    Modify UpdatePostTests into ReplacePostTests Add UpdatePostTest

--- src/test/java/restassured/learning/UpdatePostTest.java
+++ src/test/java/restassured/learning/UpdatePostTest.java
@@ -4,45 +4,22 @@ import io.restassured.http.ContentType;
 import jsonplaceholder.tests.model.Post;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import static io.restassured.RestAssured.given;
 
 public class UpdatePostTest {
 
     @Test
-    public void updatePost() {
-        Map<String, Object> newPost = new HashMap<>();
-        newPost.put("userId", 66);
-        newPost.put("title", "title after update");
-        newPost.put("body", "body after update");
-
-        given()
-                .log().all()
-                .contentType(ContentType.JSON)
-                .body(newPost)
-        .when()
-                .put("https://jsonplaceholder.typicode.com/posts/10")
-        .then()
-                .log().all();
-    }
-
-    @Test
     public void updatePostObject() {
         Post newPost = new Post();
-        newPost.setUserId(23);
         newPost.setTitle("title after update object");
-        newPost.setBody("body after update object");
 
         given()
                 .log().all()
                 .contentType(ContentType.JSON)
                 .body(newPost)
         .when()
-                .put("https://jsonplaceholder.typicode.com/posts/10")
+                .patch("https://jsonplaceholder.typicode.com/posts/10")
         .then()
                 .log().all();
     }
 }
-
